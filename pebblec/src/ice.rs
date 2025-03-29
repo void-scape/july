130dbe418a5a4a5cf93468ad57d46e648a599475
green-ogre <109698029+void-scape@users.noreply.github.com>
2025-03-29 00:01:25 -0600

    Fixed ci

---
+++ pebblec/src/ice.rs
@@ -0,0 +1,78 @@
+use pebblec_parse::lex::io;
+use std::panic::UnwindSafe;
+use std::sync::{Arc, LazyLock, Mutex};
+use std::time::UNIX_EPOCH;
+
+static BACKTRACE: LazyLock<Arc<Mutex<Option<String>>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(None)));
+
+pub fn reported_panic<Out>(capture: bool, f: impl FnOnce() -> Out + UnwindSafe) -> Option<Out> {
+    std::panic::set_hook(Box::new(panic_hook));
+
+    if capture {
+        // capture compiler output in case of panic
+        std::io::set_output_capture(Some(Default::default()));
+
+        let result = std::panic::catch_unwind(move || f());
+
+        let capture = std::io::set_output_capture(None);
+        let captured = Arc::try_unwrap(capture.unwrap())
+            .unwrap()
+            .into_inner()
+            .unwrap();
+        let output = String::from_utf8(captured).unwrap();
+
+        print!("{output}");
+        match result {
+            Ok(result) => Some(result),
+            Err(_) => {
+                report_panic(&output);
+                None
+            }
+        }
+    } else {
+        match std::panic::catch_unwind(move || f()) {
+            Ok(result) => Some(result),
+            Err(_) => {
+                report_panic("");
+                None
+            }
+        }
+    }
+}
+
+fn retrieve_backtrace() -> String {
+    format!(
+        "\nBacktrace:\n{}",
+        BACKTRACE
+            .lock()
+            .unwrap()
+            .take()
+            .unwrap_or("<Backtrace not found>".to_string())
+    )
+}
+
+fn report_panic(stdout: &str) {
+    let ice_file = format!(
+        "ICE-{}",
+        std::time::SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+    );
+
+    let note = "note: compiler encountered unexpected panic, this is a bug";
+    let ice = format!("note: reported in `{ice_file}`");
+    let notes = format!("\n{note}\n{ice}");
+    println!("{notes}");
+
+    let err = retrieve_backtrace();
+    if let Err(io_err) = io::write(ice_file, format!("{stdout}{err}{notes}").as_bytes()) {
+        println!("failed to report ICE: {io_err}");
+    }
+}
+
+fn panic_hook(info: &std::panic::PanicHookInfo) {
+    println!("{info}");
+    *BACKTRACE.lock().unwrap() = Some(std::backtrace::Backtrace::force_capture().to_string());
+}
